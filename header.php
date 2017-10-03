81ae033dc4e6b840d9aec115f0acaefdc69b65f1
Shivam Kumar <takshak.1996@gmail.com>
2017-10-03 14:21:02 +0530

    added favicon, responsive carousel and  ab aur kya kru me huh!

--- header.php
+++ header.php
@@ -4,6 +4,9 @@
   <meta charset="utf-8">
   <title>IgnouSite</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- <link rel="icon" type="image/png" href="images/fevicon/favicon-32x32.png" sizes="32x32" />
+  <link rel="icon" type="image/png" href="images/favicon/favicon-16x16.png" sizes="16x16" /> -->
+  <link rel="shortcut icon" href="images/favicon/favicon.png" type="image/x-icon" />
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="css/style.css">
   <script src="jquery.js"></script>
