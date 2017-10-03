c3f581160c53184f7c133444ffd901f55d8c3ebd
Shivam Kumar <takshak.1996@gmail.com>
2017-10-03 20:39:37 +0530

    updated aboutus.php, added ignousite icon

--- aboutus.php
+++ aboutus.php
@@ -1,7 +1,33 @@
 <!-- # Header is fetched -->
 <?php include('header.php'); ?>
 
-<h2>This is about section.</h2>
+
+<div class="container-fluid about_container">
+    <h1 class="about_h1">Get to know us a little</h1>
+</div>
+
+<div class="row row_about">
+  <div class="col-md-6 col-sm-6 col-xs-12">
+    <div class="about_img center">
+      <img class="img-responsive" src="images/ignousite-iconn.png" alt="Ignousite.com icon">
+    </div>
+  </div>
+  <div class="col-md-6 col-sm-6 col-xs-12" style="background-color: #fff;">
+    <div class="about_paragraph">
+      <p>Hi there,<br><br> We are team of ignousite. We all are not so very smart and are very lazy guys. We hate to code and hardwork (Just heard this word) but there is something that we're really fasinate about that is 'IGNOU'.</p>
+      <p>We owe the university alot. It has completly changed our life and exposed us to the world of IT. So here we are, trying to do something that we love to do. </p>
+      <p>For the IgnouSite, this is just a begining path. We have a broader vision for this website and need your support and good wishes. <br><br>Thank You! <br>Team IgnouSite</p>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-md-6 col-sm- col-xs-6">
+
+  </div>
+</div>
+
+
 
 
 <!-- # Footer is fetched -->
