c61d98da980c27d78ea3c8e8f17c6189726975da
Mintu Sharma <mintusharma24@gmail.com>
2017-10-04 07:58:44 +0530

    Bekar hai yar
    jtak commit na kru css reflect hi nhi krta

--- assignments.php
+++ assignments.php
@@ -1,106 +1,32 @@
-
-  <!--    Navigation Bar Start Here-->
+ <!--    Navigation Bar Start Here-->
   <?php include('header.php'); ?>
   <!--    Navigation bar End here        -->
   <div class="col-sm-12 assignment">
     <h1>Assignments</h1>
   </div>
-  <div class="col-sm-12 card_views">
+<div class="col-sm-12 card_views">
     <div class="col-sm-12 card_container">
       <div class="col-sm-12 bca"><span style="font-size:3.5em;color:red;">BCA</span></div>
       <div class="col-sm-3 sub_cards">
         <div class="col-sm-12 card_image">
             <span style="font-size:2.5em;margin-left:70px; margin-top:52%;">1st SEMESTER</span>
           </div>
-        <div class="col-sm-12 card_button">
-           <button type="button"  onclick="window.location.href='https://webservices.ignou.ac.in/assignments/bca/2017-18/BCA%20SEMESTER-I_2.pdf'" class="btn  btn-primary btn-block">July - January ( 2017 - 2018 )</button>
-            <button type="submit" onclick="window.location.href='https://webservices.ignou.ac.in/assignments/bca/2016-17/BCA%201st%20Sem%20Assignment%202016-17.pdf'" class="btn  btn-primary btn-block">July - January ( 2016 - 2017 )</button>
+          <div class="col-sm-12 card_button">
+           
+           dfds
         </div>
-      </div>
-      <div class="col-sm-3 sub_cards">
-        <div class="col-sm-12 card_image"> <span style="font-size:2.5em;margin-left:70px; margin-top:52%;">2nd SEMESTER</span></div>
-        <div class="col-sm-12 card_button">
-          <button type="submit" formtarget="_blank"  class="btn  btn-primary btn-block">July - January ( 2017 - 2018 )</button>
-            <button type="button" class="btn  btn-primary btn-block">July - January ( 2016 - 2017 )</button>
-        </div>
-      </div>
-      <div class="col-sm-3 sub_cards">
-        <div class="col-sm-12 card_image"> <span style="font-size:2.5em;margin-left:70px; margin-top:52%;">3rd SEMESTER</span></div>
-        <div class="col-sm-12 card_button">
-          <button type="button" class="btn  btn-primary btn-block">July - January ( 2017 - 2018 )</button>
-            <button type="button" class="btn  btn-primary btn-block">July - January ( 2016 - 2017 )</button>
-        </div>
-      </div>
-      <div class="col-sm-3 sub_cards">
-        <div class="col-sm-12 card_image"> <span style="font-size:2.5em;margin-left:70px; margin-top:52%;">4th SEMESTER</span></div>
-        <div class="col-sm-12 card_button">
-          <button type="button" class="btn  btn-primary btn-block">July - January ( 2017 - 2018 )</button>
-            <button type="button" class="btn  btn-primary btn-block">July - January ( 2016 - 2017 )</button>
-        </div>
-      </div>
-      <div class="col-sm-3 sub_cards">
-        <div class="col-sm-12 card_image"> <span style="font-size:2.5em;margin-left:70px; margin-top:52%;">5th SEMESTER</span></div>
-        <div class="col-sm-12 card_button">
-          <button type="button" class="btn  btn-primary btn-block">July - January ( 2017 - 2018 )</button>
-            <button type="button" class="btn  btn-primary btn-block">July - January ( 2016 - 2017 )</button>
-        </div>
-      </div>
-      <div class="col-sm-3 sub_cards">
-        <div class="col-sm-12 card_image"> <span style="font-size:2.5em;margin-left:70px; margin-top:52%;">6th SEMESTER</span></div>
-        <div class="col-sm-12 card_button">
-          <button type="button" class="btn  btn-primary btn-block">July - January ( 2017 - 2018 )</button>
-            <button type="button" class="btn  btn-primary btn-block">July - January ( 2016 - 2017 )</button>
-        </div>
-      </div>
-      <div class="col-sm-12 mca"><span style="font-size:3.5em;color:red;">MCA</span></div>
+          </div>
+        <div class="col-sm-12 mca"><span style="font-size:3.5em;color:red;">MCA</span></div>
       <div class="col-sm-3 sub_cards">
         <div class="col-sm-12 card_image">
             <span style="font-size:2.5em;margin-left:70px; margin-top:52%;">1st SEMESTER</span>
           </div>
         <div class="col-sm-12 card_button">
-           <button type="button"  onclick="window.location.href='https://webservices.ignou.ac.in/assignments/bca/2017-18/BCA%20SEMESTER-I_2.pdf'" class="btn  btn-primary btn-block">July - January ( 2017 - 2018 )</button>
-            <button type="submit" onclick="window.location.href='https://webservices.ignou.ac.in/assignments/bca/2016-17/BCA%201st%20Sem%20Assignment%202016-17.pdf'" class="btn  btn-primary btn-block">July - January ( 2016 - 2017 )</button>
+           
+            
         </div>
       </div>
-      <div class="col-sm-3 sub_cards">
-        <div class="col-sm-12 card_image"> <span style="font-size:2.5em;margin-left:70px; margin-top:52%;">2nd SEMESTER</span></div>
-        <div class="col-sm-12 card_button">
-          <button type="submit" formtarget="_blank"  class="btn  btn-primary btn-block">July - January ( 2017 - 2018 )</button>
-            <button type="button" class="btn  btn-primary btn-block">July - January ( 2016 - 2017 )</button>
-        </div>
-      </div>
-      <div class="col-sm-3 sub_cards">
-        <div class="col-sm-12 card_image"> <span style="font-size:2.5em;margin-left:70px; margin-top:52%;">3rd SEMESTER</span></div>
-        <div class="col-sm-12 card_button">
-          <button type="button" class="btn  btn-primary btn-block">July - January ( 2017 - 2018 )</button>
-            <button type="button" class="btn  btn-primary btn-block">July - January ( 2016 - 2017 )</button>
-        </div>
-      </div>
-      <div class="col-sm-3 sub_cards">
-        <div class="col-sm-12 card_image"> <span style="font-size:2.5em;margin-left:70px; margin-top:52%;">4th SEMESTER</span></div>
-        <div class="col-sm-12 card_button">
-          <button type="button" class="btn  btn-primary btn-block">July - January ( 2017 - 2018 )</button>
-            <button type="button" class="btn  btn-primary btn-block">July - January ( 2016 - 2017 )</button>
-        </div>
-      </div>
-      <div class="col-sm-3 sub_cards">
-        <div class="col-sm-12 card_image"> <span style="font-size:2.5em;margin-left:70px; margin-top:52%;">5th SEMESTER</span></div>
-        <div class="col-sm-12 card_button">
-          <button type="button" class="btn  btn-primary btn-block">July - January ( 2017 - 2018 )</button>
-            <button type="button" class="btn  btn-primary btn-block">July - January ( 2016 - 2017 )</button>
-        </div>
-      </div>
-      <div class="col-sm-3 sub_cards">
-        <div class="col-sm-12 card_image"> <span style="font-size:2.5em;margin-left:70px; margin-top:52%;">6th SEMESTER</span></div>
-        <div class="col-sm-12 card_button">
-          <button type="button" class="btn  btn-primary btn-block">July - January ( 2017 - 2018 )</button>
-            <button type="button" class="btn  btn-primary btn-block">July - January ( 2016 - 2017 )</button>
-        </div>
-      </div>
-      <div class="col-sm-12 mca"><button type="button" class="btn  btn-primary btn-block">FOR OTHER COURSES CLICK HERE</button></div>
+        <div class="col-sm-12 mca"><button type="button" onclick="window.location.href='https://webservices.ignou.ac.in/assignments'" class="btn  btn-primary btn-block">FOR OTHER COURSES CLICK HERE</button></div>
   
       </div>
     </div>
-
- <!-- Footer is fetched -->
-  <?php include('footer.php'); ?>
