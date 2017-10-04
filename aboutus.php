ca67315f6de7268580869a78fca5032ff49e634b
Shivam Kumar <takshak.1996@gmail.com>
2017-10-04 22:44:30 +0530

    updated aboutus.php, added new fb icon

--- aboutus.php
+++ aboutus.php
@@ -1,6 +1,11 @@
 <!-- # Header is fetched -->
 <?php include('header.php'); ?>
 
+<?php
+  $teamMembers =  array('Shivam Rajput', 'Ishminder kharbanda', 'Mintu Sharma');
+  $teamMembersDescription = array('He is a geek, having 6GB RAM in his Linux machine and very lazy dude who likes to be in his room.');
+?>
+
 
 <div class="container-fluid about_container">
   <h1 class="about_h1"><?php echo ucfirst("Get to know us a little"); ?></h1>
@@ -29,37 +34,37 @@
     <div class="row">
       <div class="col-md-2 col-sm-3 col-xs-4">
         <div class="social_profiles">
-          <a href="#" data-toggle="tooltip" title="Ignousite On Github"><img src="images/github.svg" class="img-responsive"  id="hovers" alt="ignousite on Github"></a>
+          <a href="#" data-toggle="tooltip" data-placement="bottom" title="Ignousite On Github"><img src="images/github.svg" class="img-responsive"  id="hovers" alt="ignousite on Github"></a>
         </div>
       </div>
 
       <div class="col-md-2 col-sm-3 col-xs-4">
         <div class="social_profiles">
-          <a href="#" data-toggle="tooltip" title="Ignousite On Stackoverflow"><img src="images/stackoverflow.svg" class="img-responsive" alt="ignousite on Stackoverflow"></a>
+          <a href="#" data-toggle="tooltip" data-placement="bottom" title="Ignousite On Stackoverflow"><img src="images/stackoverflow.svg" class="img-responsive" alt="ignousite on Stackoverflow"></a>
         </div>
       </div>
 
       <div class="col-md-2 col-sm-3 col-xs-4">
         <div class="social_profiles">
-          <a href="#" data-toggle="tooltip" title="Ignousite On Telegram"><img src="images/telegram.svg" class="img-responsive" alt="Ignousite on telegram"></a>
+          <a href="#" data-toggle="tooltip" data-placement="bottom" title="Ignousite On Telegram"><img src="images/telegram.svg" class="img-responsive" alt="Ignousite on telegram"></a>
         </div>
       </div>
 
       <div class="col-md-2 col-sm-3 col-xs-4">
         <div class="social_profiles">
-          <a href="#" data-toggle="tooltip" title="Ignousite On Facebook"><img src="images/facebook.svg" class="img-responsive" alt="Ignousite on Facebook"></a>
+          <a href="#" data-toggle="tooltip" data-placement="bottom" title="Ignousite On Facebook"><img src="images/facebook.svg" class="img-responsive" alt="Ignousite on Facebook"></a>
         </div>
       </div>
 
       <div class="col-md-2 col-sm-3 col-xs-4">
         <div class="social_profiles">
-          <a href="#" data-toggle="tooltip" title="Ignousite On Twitter"><img src="images/twitter.svg" class="img-responsive" alt="Ignousite on Twitter"></a>
+          <a href="#" data-toggle="tooltip" data-placement="bottom" title="Ignousite On Twitter"><img src="images/twitter.svg" class="img-responsive" alt="Ignousite on Twitter"></a>
         </div>
       </div>
 
       <div class="col-md-2 col-sm-3 col-xs-4">
         <div class="social_profiles">
-          <a href="#" data-toggle="tooltip" title="Ignousite On Youtube"><img src="images/youtube.svg" class="img-responsive" alt="Ignousite on Youtube"></a>
+          <a href="#" data-toggle="tooltip" data-placement="bottom" title="Ignousite On Youtube"><img src="images/youtube.svg" class="img-responsive" alt="Ignousite on Youtube"></a>
         </div>
       </div>
     </div>
@@ -67,6 +72,58 @@
   </div>
 
 </div>
+
+<!-- Team section starts here -->
+<div class="row row_about" style="background-color:#f8f8f8; padding-bottom:60px;">
+  <div class="social_profiles_heading"><h2 style="color:#2d4788;"><?php echo ucfirst("Ignousite Team"); ?></h2></div>
+  <!-- <hr> -->
+  <div class="col-md-2"></div>
+  <div class="col-md-8" style="padding-bottom:15px;">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="row">
+
+          <div class="col-md-4">
+            <div class="card team_card">
+              <img class="card-img-top team_card_img" src="images/ignousite-iconn.png" alt="Card image cap">
+              <div class="card-body team_card_body">
+                <h4 class="card-title"><?php echo $teamMembers[0]; ?></h4>
+                <p class="card-text"><?php echo $teamMembersDescription[0]; ?></p>
+                <a href="#" class="btn btn-primary">Linkedin Profile</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="card team_card">
+              <img class="card-img-top team_card_img" src="images/ignousite-iconn.png" alt="Card image cap">
+              <div class="card-body">
+                <h4 class="card-title"><?php echo $teamMembers[1]; ?></h4>
+                <p class="card-text"><?php echo $teamMembersDescription[0]; ?></p>
+                <a href="#" class="btn btn-primary">Linkedin Profile</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="card team_card">
+              <img class="card-img-top team_card_img" src="images/ignousite-iconn.png" alt="Card image cap">
+              <div class="card-body">
+                <h4 class="card-title"><?php echo $teamMembers[2]; ?></h4>
+                <p class="card-text"><?php echo $teamMembersDescription[0]; ?></p>
+                <a href="#" class="btn btn-primary">Linkedin Profile</a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <div class="col-md-2"></div>
+  </div>
+
+</div>
+<!-- Team section ends here -->
 
 
 
