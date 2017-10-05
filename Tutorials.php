6523a606dc543c48df65abd4dcaedb61941a8c6a
ishminder <ishmindersingh1996@gmail.com>
2017-10-05 23:45:39 +0530

    TUTORIAL PAGE AS PER MINTU JI'S REQUIREMENT
    MORE WORK TO BE DONE...
    I WILL CONTINUE UPDATION ON THIS PAGE TOMORROW

--- Tutorials.php
+++ Tutorials.php
@@ -3,49 +3,79 @@
 <?php include('header.php'); ?>
 
   <!-- Cards Starting From Here  -->
-  <div class="col-sm-12 tutorials">
-<!--    <h1>Tutorials</h1>-->
-  </div>
-<div class="col-sm-12 bca">Quick Tutorials</div>
-<div class="col-sm-12 card_views">
-    <div class="col-sm-12 card_container">
-<div class="col-sm-3 sub_cards2">
-        <div class="col-sm-12 card_image2"><img class="col-sm-12" src="images/c.jpg"/></div>
-        <div class="col-sm-12 card_button2">
-          <button type="button" class="btn btn-primary btn-block"><h3>C Tutorials</h3></button>
+<div class="row">
+
+    <div class="col-sm-12 bca">
+    Tutorials
+    </div> 
+<div class="col-md-4 col-sm-6 ">
+          <div class="thumbnail">
+              <div class="card_image">
+            <img src="images/image1.svg">
+            </div>      
+            <div class="card_button">
+                <a href="" target="_blank"><button type="button" class="btn  btn-primary btn-block">C PROGRAMMING</button></a>
+            
+            </div>  
+          </div>
         </div>
-      </div>
-        <div class="col-sm-3 sub_cards2">
-        <div class="col-sm-12 card_image2"><img class="col-sm-12" src="images/c2.jpg"/></div>
-        <div class="col-sm-12 card_button2">
-          <button type="button" class="btn  btn-primary btn-block"><h3>C# Tutorials</h3></button>
+    <div class="col-md-4 col-sm-6 ">
+          <div class="thumbnail">
+              <div class="card_image">
+            <img src="images/image1.svg">
+            </div>      
+            <div class="card_button">
+                <a href="" target="_blank"><button type="button" class="btn  btn-primary btn-block">C++ PROGRAMMING</button></a>
+            
+            </div>  
+          </div>
         </div>
-      </div>
-      <div class="col-sm-3 sub_cards2">
-        <div class="col-sm-12 card_image2"><img class="col-sm-12" src="images/images.png"/></div>
-        <div class="col-sm-12 card_button2">
-          <button type="button" class="btn btn-primary btn-block"><h3>C++ Tutorials</h3></button>
+    <div class="col-md-4 col-sm-6 ">
+          <div class="thumbnail">
+              <div class="card_image">
+            <img src="images/image1.svg">
+            </div>      
+            <div class="card_button">
+                <a href="" target="_blank"><button type="button" class="btn  btn-primary btn-block">C# PROGRAMMING</button></a>
+          
+          </div>
         </div>
-      </div>
-      <div class="col-sm-3 sub_cards2">
-        <div class="col-sm-12 card_image2"><img class="col-sm-12" src="images/c2.jpg"/></div>
-        <div class="col-sm-12 card_button2">
-          <button type="button" class="btn btn-primary btn-block"><h3>Python Tutorials</h3></button>
-        </div>
-      </div>
-      <div class="col-sm-3 sub_cards2">
-        <div class="col-sm-12 card_image2"><img class="col-sm-12" src="images/images.png"/></div>
-        <div class="col-sm-12 card_button2">
-          <button type="button" class="btn btn-primary btn-block"><h3>Java Tutorials</h3></button>
-        </div>
-      </div>
-      <div class="col-sm-3 sub_cards2">
-        <div class="col-sm-12 card_image2"><img class="col-sm-12" src="images/c.jpg"/></div>
-        <div class="col-sm-12 card_button2">
-          <button type="button" class="btn btn-primary btn-block"><h3>Spring Tutorials</h3></button>
-        </div>
-      </div>
     </div>
-</div>
+    <div class="col-md-4 col-sm-6">
+          <div class="thumbnail">
+              <div class="card_image">
+            <img src="images/image1.svg">
+            </div>      
+            <div class="card_button">
+                <a href="" target="_blank"><button type="button" class="btn  btn-primary btn-block">JAVA PROGRAMMING</button></a>
+          
+            </div>  
+          </div>
+        </div>
+    <div class="col-md-4 col-sm-6">
+          <div class="thumbnail">
+              <div class="card_image">
+            <img src="images/image1.svg">
+            </div>      
+            <div class="card_button">
+                <a href="" target="_blank"><button type="button" class="btn  btn-primary btn-block">PYTHON PROGRAMMING</button></a>
+           
+            </div>  
+          </div>
+        </div>
+    <div class="col-md-4 col-sm-6">
+          <div class="thumbnail">
+              <div class="card_image">
+            <img src="images/image1.svg">
+            </div>      
+            <div class="card_button">
+                <a href="" target="_blank"><button type="button" class="btn  btn-primary btn-block">PHP WEB DEVELOPMENT</button></a>
+          
+            </div>  
+          </div>
+        </div>
+    </div>
+    
+
       <!-- Footer is fetched -->
        <?php include('footer.php'); ?>
