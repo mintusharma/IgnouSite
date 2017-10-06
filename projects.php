20dbd642615aacfd4283a386a99166f6abfa03d4
Mintu Sharma <mintusharma24@gmail.com>
2017-10-06 09:24:07 +0530

    Check Project Page Now
    Is there any correction contact me at my no.

--- projects.php
+++ projects.php
@@ -9,9 +9,9 @@
 
       <div class="panel-group" id="accordion">
         <div class="panel panel-default">
-          <div class="panel-heading">
+          <div class="panel-heading ">
             <h2 class="panel-title">
-              <a data-toggle="collapse" class="btn2 collapsed" data-parent="#accordion" href="#collapse1"> <b> Project Based On Java Language </b></a>
+              <a data-toggle="collapse" class="btn2 collapsed" data-parent="#accordion" href="#collapse1"> Projects On Java Language</a>
             </h2>
           </div>
           <div id="collapse1" class="panel-collapse collapse in">
@@ -19,7 +19,10 @@
               <li class="list-group-item">Online Examination System</li>
               <li class="list-group-item">Medical Store (Online Medical Store Management System) </li>
               <li class="list-group-item">Book Store (Online Book Store Management System)</li>
+                 <li class="list-group-item">Online Matrimonial Website</li>
+                <li class="list-group-item">E-Learning Website</li> 
               <li class="list-group-item">Library Management System</li>
+                  <li class="list-group-item">Online Admission System</li>
               <li class="list-group-item">School Management System</li>
             </ul>
           </div>
@@ -27,70 +30,82 @@
         <div class="panel panel-default">
           <div class="panel-heading">
             <h4 class="panel-title">
-              <a data-toggle="collapse" data-parent="#accordion" href="#collapse2"><span class="glyphicon glyphicon-plus"></span> <b> Project Based On PHP </b></a>
+               <a data-toggle="collapse" class="btn2 collapsed" data-parent="#accordion" href="#collapse2"> Projects On PHP</a>
             </h4>
           </div>
           <div id="collapse2" class="panel-collapse collapse">
             <ul class="list-group">
+                <li class="list-group-item">E-Commerce ( Online Shopping )</li> 
               <li class="list-group-item">Online Examination System</li>
               <li class="list-group-item">Medical Store (Online Medical Store Management System) </li>
               <li class="list-group-item">Book Store (Online Book Store Management System)</li>
+                <li class="list-group-item">Library Management System</li>
+                 <li class="list-group-item">Online Matrimonial Website</li>
+                 <li class="list-group-item">Online Admission System</li>
+                
             </ul>
           </div>
         </div>
         <div class="panel panel-default">
           <div class="panel-heading">
             <h4 class="panel-title">
-              <a data-toggle="collapse" data-parent="#accordion" href="#collapse3"><span class="glyphicon glyphicon-plus"></span> <b> Project Based On Android </b></a>
+               <a data-toggle="collapse" class="btn2 collapsed" data-parent="#accordion" href="#collapse3"> Projects On Android</a>
             </h4>
           </div>
           <div id="collapse3" class="panel-collapse collapse">
             <ul class="list-group">
-              <li class="list-group-item">Online Examination System</li>
-              <li class="list-group-item">Medical Store (Online Medical Store Management System) </li>
-              <li class="list-group-item">Book Store (Online Book Store Management System)</li>
+              <li class="list-group-item">Online Examination System App</li>
+              <li class="list-group-item">Medical Store Management System App</li>
+                <li class="list-group-item">Online Quiz App</li>
+                <li class="list-group-item">Connect (Cross and Nut Game) App</li>
+              <li class="list-group-item">Book Store App</li>
+            <li class="list-group-item">File Sharing App</li>    
             </ul>
           </div>
         </div>
         <div class="panel panel-default">
           <div class="panel-heading">
             <h4 class="panel-title">
-              <a data-toggle="collapse" data-parent="#accordion" href="#collapse4"><span class="glyphicon glyphicon-plus"></span> <b> Beginners Level Projects </b></a>
+               <a data-toggle="collapse" class="btn2 collapsed" data-parent="#accordion" href="#collapse4"> Beginners Level Projects</a>
             </h4>
           </div>
           <div id="collapse4" class="panel-collapse collapse">
             <ul class="list-group">
-              <li class="list-group-item">Online Examination System</li>
-              <li class="list-group-item">Medical Store (Online Medical Store Management System) </li>
-              <li class="list-group-item">Book Store (Online Book Store Management System)</li>
+              <li class="list-group-item">Calculator (Java / Android)</li>
+            <li class="list-group-item">Alarm App</li>
+                <li class="list-group-item">E-Book Reader App</li>
+              <li class="list-group-item">Web Browser (Java / Android)</li>
+                <li class="list-group-item">Puzzle App</li>
+                <li class="list-group-item">E-Tutorial (Web / App)</li>
+                <li class="list-group-item">Text Editor (Java / Android)</li>
             </ul>
           </div>
         </div>
         <div class="panel panel-default">
           <div class="panel-heading">
             <h4 class="panel-title">
-              <a data-toggle="collapse" data-parent="#accordion" href="#collapse5"><span class="glyphicon glyphicon-plus"></span> <b> Advanced Level Projects</b> </a>
+               <a data-toggle="collapse" class="btn2 collapsed" data-parent="#accordion" href="#collapse5"> Advanced Level Projects</a>
             </h4>
           </div>
           <div id="collapse5" class="panel-collapse collapse">
             <ul class="list-group">
-              <li class="list-group-item">Online Examination System</li>
-              <li class="list-group-item">Medical Store (Online Medical Store Management System) </li>
-              <li class="list-group-item">Book Store (Online Book Store Management System)</li>
+              <li class="list-group-item">Examination System For School Or Institute</li>
+                <li class="list-group-item">School Management System</li>
+             <li class="list-group-item">Multi-Media Player App</li>
+                <li class="list-group-item">Friends Chating App</li>
+                <li class="list-group-item">E-Commerce ( Online Shopping )</li> 
             </ul>
           </div>
         </div>
         <div class="panel panel-default">
           <div class="panel-heading">
             <h4 class="panel-title">
-              <a data-toggle="collapse" data-parent="#accordion" href="#collapse6"><span class="glyphicon glyphicon-plus"></span> <b> Other Projects </b></a>
+               <a data-toggle="collapse" class="btn2 collapsed" data-parent="#accordion" href="#collapse6"> Other Projects</a>
             </h4>
           </div>
           <div id="collapse6" class="panel-collapse collapse">
             <ul class="list-group">
-              <li class="list-group-item">Online Examination System</li>
-              <li class="list-group-item">Medical Store (Online Medical Store Management System) </li>
-              <li class="list-group-item">Book Store (Online Book Store Management System)</li>
+                <li class="list-group-item">For any Other Projects Contact Us</li>
             </ul>
           </div>
         </div>
@@ -133,7 +148,11 @@
       </form>
     </div>
   </div>
-
+<div class="col-sm-12  imp_note">
+<div class="subnote">
+    All the Projects are availabe at lowest price..  >>
+    </div>
+</div>
 
   <!-- Footer is fetched -->
   <?php include('footer.php'); ?>
