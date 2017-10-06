762a7765e94ad7310b0df9bb1983414ea2501068
Shivam Kumar <takshak.1996@gmail.com>
2017-10-06 13:06:52 +0530

    Hover effect added in about social icons and time restored _D

--- js/ignousite.js
+++ js/ignousite.js
@@ -15,7 +15,7 @@ var x = setInterval(function() {  //This function updates the date every second
 
   document.getElementById("doomsDay").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-  if(days < 1){
+  if(hours < 1){
     clearInterval(x);
     document.getElementById("doomsDay").innerHTML = "DOOMSDAY!";
   }
