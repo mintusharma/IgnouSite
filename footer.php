53b6f79c9138dd1e19c4a2a3b06b7570bf2ccd2f
Shivam Kumar <takshak.1996@gmail.com>
2017-10-06 14:55:09 +0530

    Added footer Balle-N-Balle or tum khao dahi balle

--- footer.php
+++ footer.php
@@ -1,11 +1,51 @@
-<div class="col-sm-12" style="margin: 0; padding: 0;"> <!-- footer Start -->
+<!-- footer Start -->
+<footer>
+  <div class="row">
+    <div class="col-md-12">
+      <div class="footer_div">
+        <a href="#"><img src="images/github.svg" alt="Ignousite on github"></a>
+        <a href="#"><img src="images/stackoverflow.svg" alt="Ignousite on stackoverflow"></a>
+        <a href="#"><img src="images/telegram.svg" alt="Ignousite on telegram"></a>
+        <a href="#"><img src="images/facebook.svg" alt="Ignousite on facebook"></a>
+        <a href="#"><img src="images/twitter.svg" alt="Ignousite on twitter"></a>
+        <a href="#"><img src="images/youtube.svg" alt="Ignousite on youtube"></a>
+      </div>
+      <div class="footer_social_links">
+        <a href="#">HOME</a>
+        <a href="#">ASSIGNMENTS</a>
+        <a href="#">PROJECTS</a>
+        <a href="#">DOWNLOADS</a>
+        <a href="#">TUTORIALS</a>
+        <a href="#">CONTACT</a>
+        <a href="#">ABOUT</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-12 footer_bottom">
+      <p>Copyright (c) 2017 <a href="/index.php">IGNOUSITE.COM</a> All Rights Reserved.</p>
+    </div>
+  </div>
+</footer>
+
+
+
+
+
+
+
+
+
+<!-- <div class="col-sm-12" style="margin: 0; padding: 0;">
   <footer class="text-center footertext real_footer_text">
     <a class="up-arrow" href="#" data-toggle="tooltip" title="TO TOP">
       <span class="glyphicon glyphicon-chevron-up"></span>
     </a><br><br>
     <p>ignousite - It's All about IGNOU</p>
   </footer>
-</div> <!-- footer ends -->
+</div>  -->
+<!-- footer ends -->
 
 </body>
 </html>
